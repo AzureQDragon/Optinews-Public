751384f97f16a1ec8a40a0922e6df1f27b85ffe6
Daniel <dm32@illinois.edu>
2020-10-05 19:32:51 -0500

    fix: update function names and return call

--- backend/app.py
+++ backend/app.py
@@ -3,32 +3,30 @@ import requests
 from newsapi import NewsApiClient
 from monkeylearn import MonkeyLearn
 
-# ml = MonkeyLearn('4c67b4c62c390c6c14cde310bda788b0a3fe0639')
-# modelid = "cl_pi3C7JiL"
-# newsapi = NewsApiClient(api_key='***REMOVED***')
-# top_headlines = newsapi.get_top_headlines(country='us',
-#                                           language='en')
+ml = MonkeyLearn('4c67b4c62c390c6c14cde310bda788b0a3fe0639')
+modelid = "cl_pi3C7JiL"
+newsapi = NewsApiClient(api_key='***REMOVED***')
+top_headlines = newsapi.get_top_headlines(country='us',
+                                          language='en')
 
-# articles = {}
-# data = []
+articles = []
+data = []
 
 
-# for i in range(len(top_headlines["articles"])):
-#     data = [top_headlines["articles"][i]["content"]]
-#     if data == [None]:
-#         data = [top_headlines["articles"][i]["description"]]
-#     if data == [None]:
-#         data = [top_headlines["articles"][i]["title"]]
-#     result = ml.classifiers.classify(modelid, data)
-#     if(result.body[0]["classifications"][0]["tag_name"] == "Positive" and result.body[0]["classifications"][0]["confidence"] >= .7):
-#         articles.append(top_headlines["articles"][i])
+for i in range(len(top_headlines["articles"])):
+    data = [top_headlines["articles"][i]["content"]]
+    if data == [None]:
+        data = [top_headlines["articles"][i]["description"]]
+    if data == [None]:
+        data = [top_headlines["articles"][i]["title"]]
+    result = ml.classifiers.classify(modelid, data)
+    if(result.body[0]["classifications"][0]["tag_name"] == "Positive" and result.body[0]["classifications"][0]["confidence"] >= .7):
+        articles.append(top_headlines["articles"][i])
 
-#print(articles)
-
-article = ["help me"]
+print(articles)
 
 app = Flask(__name__)
 
 @app.route('/')
-def articles():
-    return article[0]
+def firstarticle():
+    return articles[0]
