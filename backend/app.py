baa3645a1de83ec3c5119c5c2bd0944074e0fa7c
Jen <jen.schwabe2@gmail.com>
2020-10-05 21:04:30 -0500

    Fixed a few issues

--- backend/app.py
+++ backend/app.py
@@ -3,27 +3,27 @@ import requests
 from newsapi import NewsApiClient
 from monkeylearn import MonkeyLearn
 
-# ml = MonkeyLearn('4c67b4c62c390c6c14cde310bda788b0a3fe0639')
-# modelid = "cl_pi3C7JiL"
-# newsapi = NewsApiClient(api_key='***REMOVED***')
-# top_headlines = newsapi.get_top_headlines(country='us',
-#                                           language='en')
+ml = MonkeyLearn('ee37b454bc5c52973ec638bddbce7058882e0dbd')
+modelid = "cl_pi3C7JiL"
+newsapi = NewsApiClient(api_key='***REMOVED***')
+top_headlines = newsapi.get_top_headlines(country='us',
+                                          language='en')
 
-# articles = {}
-# data = []
+articles = []
+data = []
 
 
-# for i in range(len(top_headlines["articles"])):
-#     data = [top_headlines["articles"][i]["content"]]
-#     if data == [None]:
-#         data = [top_headlines["articles"][i]["description"]]
-#     if data == [None]:
-#         data = [top_headlines["articles"][i]["title"]]
-#     result = ml.classifiers.classify(modelid, data)
-#     if(result.body[0]["classifications"][0]["tag_name"] == "Positive" and result.body[0]["classifications"][0]["confidence"] >= .7):
-#         articles.append(top_headlines["articles"][i])
+for i in range(len(top_headlines["articles"])):
+    data = [top_headlines["articles"][i]["content"]]
+    if data == [None]:
+        data = [top_headlines["articles"][i]["description"]]
+    if data == [None]:
+        data = [top_headlines["articles"][i]["title"]]
+    result = ml.classifiers.classify(modelid, data)
+    if(result.body[0]["classifications"][0]["tag_name"] == "Positive" and result.body[0]["classifications"][0]["confidence"] >= .7):
+        articles.append(top_headlines["articles"][i]["title"])
 
-#print(articles)
+print(articles)
 
 article = ["help me"]
 
