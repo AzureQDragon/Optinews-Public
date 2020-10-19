a5e0e005f6059c58aa317bfa7a724cb8bc71459a
raynaw2 <rwuh2@illinois.edu>
2020-10-19 10:19:12 -0500

    Revert "Merge branch 'frontend' of https://github.com/CS196Illinois/group-1 into frontend"
    This reverts commit 6e7e1af8898c3bf76e7deaa1e3e12be6a901ba2c, reversing
    changes made to 0935790ef939b558d279d668ecac40e75e743bac.

--- optinews/src/App.js
+++ optinews/src/App.js
@@ -1,30 +1,24 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './Components/NavBar';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import NewsCard from './Components/NewsCard';
-import FilterCard from './Components/FilterCard';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className='Body'>
-      <NavBar />
-      <Container>
-        <Row>
-          <Col>
-            <FilterCard />
-          </Col>
-          <Col>
-            <NewsCard />
-            <NewsCard />
-            <NewsCard />
-            <NewsCard />
-          </Col>
-        </Row>
-      </Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
